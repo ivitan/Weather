fc0671276b5165d54cae787d84a2e230c7a1647c
Vitan <vitan.me@gmail.com>
2020-12-22 09:23:42 +0800

    🐛 FIX: format

--- GetWeather.py
+++ GetWeather.py
@@ -3,25 +3,30 @@ from urllib import request
 import re
 
 url_sh = 'http://www.weather.com.cn/weather/101280101.shtml'
-re_seven_days = re.compile(r'<h1>([1-9].*)</h1>\n.*\n.*\n.*class="wea">(.*)</p>\n.*\n<span>([0-9]*).*>([0-9].*)</i>\n')
+re_seven_days = re.compile(
+    r'<h1>([1-9].*)</h1>\n.*\n.*\n.*class="wea">(.*)</p>\n.*\n<span>([0-9]*).*>([0-9].*)</i>\n')
 
 # re_tonight = re.compile(r'<h1>([1-9].*)</h1>\n.*\n.*\n.*class="wea">(.*)</p>\n.*\n.*<i>([0-9].*)</i>\n')
 
+
 def get_weekly_weather(url=url_sh):
-	req = request.Request(url)
-	weekly_weather = request.urlopen(req).read().decode('utf-8')
-	return weekly_weather
+    req = request.Request(url)
+    weekly_weather = request.urlopen(req).read().decode('utf-8')
+    return weekly_weather
+
 
 def get_weather(weekly_weather, index=2):
-	seven_days = re_seven_days.findall(weekly_weather)
-	# tonight = re_tonight.findall(weekly_weather)
-	# if tonight:
-	# 	print('\n', tonight[0][0].replace('日', '号'), tonight[0][1], tonight[0][2], '\n')
-	if index < 1 and index > 7:
-		index = 2
-	for i in range(index):
-		day = seven_days[i]
-		print('\n', day[0].replace('日', '号'), day[1], day[2], '~', day[3], '\n')
-	return 1
+    seven_days = re_seven_days.findall(weekly_weather)
+    # tonight = re_tonight.findall(weekly_weather)
+    # if tonight:
+    # 	print('\n', tonight[0][0].replace('日', '号'), tonight[0][1], tonight[0][2], '\n')
+    if index < 1 and index > 7:
+        index = 2
+    for i in range(index):
+        day = seven_days[i]
+        print('\n', day[0].replace('日', '号'),
+              day[1], day[2], '~', day[3], '\n')
+    return 1
+
 
 get_weather(get_weekly_weather())
--- weather.py
+++ weather.py
@@ -2,61 +2,66 @@
 from urllib.parse import urlencode
 import requests
 import os
- 
+
+
 def GetWeather(ct):
     # 天气接口
-    url="https://tianqiapi.com/api"
+    url = "https://tianqiapi.com/api"
     # 参数
     apiID = os.environ['apiID']
     appSecret = os.environ['appSecret']
-    data={}
-    data['version']='v6'
-    data['appid']= apiID
-    data['appsecret']= appSecret
-    data['city']=ct
- 
+    data = {}
+    data['version'] = 'v6'
+    data['appid'] = apiID
+    data['appsecret'] = appSecret
+    data['city'] = ct
+
     # 将参数转换成url可用格式
-    data=urlencode(data)
- 
+    data = urlencode(data)
+
     # 最终的url请求
-    request=url+'?'+data
- 
+    request = url+'?'+data
+
     # 读取请求结果
-    rep=requests.get(request)
- 
+    rep = requests.get(request)
+
     # 请求结果转换成json格式
     repJson = rep.json()
- 
+
     tdate = repJson.get('date')
     week = repJson.get('week')
     city = repJson.get('city')
     wea = repJson.get('wea')
-    tem1=repJson.get('tem1')
-    tem2 =repJson.get('tem2')
-    air=repJson.get('air_level')
-    pm=repJson.get('air_pm25')
-
-
+    tem1 = repJson.get('tem1')
+    tem2 = repJson.get('tem2')
+    air = repJson.get('air_level')
+    pm = repJson.get('air_pm25')
 
     text = week+' - '+wea
-    weather = '- '+city+'：'+wea+'\n'+'- '+tem2+'℃ ~ '+tem1+'℃'+'\n'+'- '+'空气质量：'+air+'\n'+'- '+'PM2.5：'+pm
+    weather = '- '+city+'：'+wea+'\n'+'- '+tem2+'℃ ~ ' + \
+        tem1+'℃'+'\n'+'- '+'空气质量：'+air+'\n'+'- '+'PM2.5：'+pm
 
-    return(text,weather)
+    return(text, weather)
+
+
 GetWeather('广州')
-# Server 酱    
+# Server 酱
 
-def SendWechat(title,message):
+
+def SendWechat(title, message):
     # text 为推送 title,desp 为推送描述
     sckey = os.environ['SCKEY']
     url = 'https://sc.ftqq.com/'+sckey+'.send?text='+title+'&desp='+message
     requests.get(url)
 
+
 def main():
     Weathers = GetWeather('广州')
     title = Weathers[0]
     message = Weathers[1]
-    print(title,message)
-    SendWechat(title,message)
+    print(title, message)
+    SendWechat(title, message)
+
 
 if __name__ == '__main__':
     main()
